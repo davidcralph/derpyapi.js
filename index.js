15bc2f4a0005611bf73be1d0d5b2218854e096ea
Dave R <davetheprogrammer2@gmail.com>
2018-05-29 12:58:40 +0100

    hi am API

---
+++ index.js
@@ -0,0 +1,27 @@
+const snekfetch = require('snekfetch');
+
+class derpy {
+     static miku {
+       return new Promise((reject, resolve) => {
+       snekfetch.get(`https://ohlookitsderpy.space/api/miku`)
+       .then((res) => resolve(res.body))
+       .catch(err => reject(err))
+       });
+   }
+   static lovelive {
+       return new Promise((reject, resolve) => {
+       snekfetch.get(`https://ohlookitsderpy.space/api/lovelive`)
+       .then((res) => resolve(res.body))
+       .catch(err => reject(err))
+       });
+   }
+    static ddlc {
+       return new Promise((reject, resolve) => {
+       snekfetch.get(`https://ohlookitsderpy.space/api/ddlc`)
+       .then((res) => resolve(res.body))
+       .catch(err => reject(err))
+       });
+   }
+}
+
+module.exports = derpy;
