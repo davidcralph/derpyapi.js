ed9aee02920014fe845eefb5a9c674c704c70996
Dave R <davetheprogrammer2@gmail.com>
2018-06-17 20:14:16 +0100

    meme code

--- index.js
+++ index.js
@@ -15,21 +15,21 @@ class derpy {
        .catch(err => reject(err))
        });
    }
-    static ddlc() {
+   static ddlc() {
        return new Promise((reject, resolve) => {
        snekfetch.get(`https://ohlookitsderpy.space/api/ddlc`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
-    static k_on() {
+   static k_on() {
        return new Promise((reject, resolve) => {
        snekfetch.get(`https://ohlookitsderpy.space/api/k_on`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
-    static takagi() {
+   static takagi() {
        return new Promise((reject, resolve) => {
        snekfetch.get(`https://ohlookitsderpy.space/api/takagi`)
        .then((res) => resolve(res.body))
