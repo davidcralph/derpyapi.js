e057111cbe303af652daf803248f8b8660faefc2
Dave R <davetheprogrammer2@gmail.com>
2018-07-27 12:06:09 +0100

    revert

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-const jaczfetch = require('jaczfetch'),
+const snekfetch = require('snekfetch'),
       baseURL = 'https://api.ohlookitsderpy.space';
 
 module.exports = class derpy {
     static miku() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/miku`)
+       snekfetch.get(`${baseURL}/miku`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static lovelive() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/lovelive`)
+       snekfetch.get(`${baseURL}/lovelive`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static k_on() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/k-on`)
+       snekfetch.get(`${baseURL}/k-on`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static ddlc() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/ddlc`)
+       snekfetch.get(`${baseURL}/ddlc`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static takagi() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/takagi`)
+       snekfetch.get(`${baseURL}/takagi`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static nekoparastatic() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/nekoparastatic`)
+       snekfetch.get(`${baseURL}/nekoparastatic`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static nekoparagif() {
        return new Promise((reject, resolve) => {
-       jaczfetch.get(`${baseURL}/nekoparagif`)
+       snekfetch.get(`${baseURL}/nekoparagif`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
