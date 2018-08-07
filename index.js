ed5413eb819a5dfadb099e4b20b9918d6c4ae6d4
Dave R <davetheprogrammer2@gmail.com>
2018-08-07 16:11:24 +0100

    yes

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-const snekfetch = require('snekfetch'),
+const fetch = require('node-superfetch'),
       baseURL = 'https://api.ohlookitsderpy.space';
 
 module.exports = class derpy {
     static miku() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/miku`)
+       fetch.get(`${baseURL}/miku`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static lovelive() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/lovelive`)
+       fetch.get(`${baseURL}/lovelive`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static k_on() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/k-on`)
+       fetch.get(`${baseURL}/k-on`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static konosuba() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/konosuba`)
+       fetch.get(`${baseURL}/konosuba`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static ddlc() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/ddlc`)
+       fetch.get(`${baseURL}/ddlc`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static takagi() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/takagi`)
+       fetch.get(`${baseURL}/takagi`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static nekoparastatic() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/nekoparastatic`)
+       fetch.get(`${baseURL}/nekoparastatic`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
    }
    static nekoparagif() {
        return new Promise((reject, resolve) => {
-       snekfetch.get(`${baseURL}/nekoparagif`)
+       fetch.get(`${baseURL}/nekoparagif`)
        .then((res) => resolve(res.body))
        .catch(err => reject(err))
        });
